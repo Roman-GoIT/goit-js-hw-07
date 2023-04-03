38f2a7ae4f97e9eed845f70e008a469f9f6919e2
Roman-GoIT <best4you.ua@gmail.com>
2023-04-03 23:43:14 +0300

    hw

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,23 +1,31 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
-console.log(galleryItems);
 
 const gallery = document.querySelector(".gallery");
+const markup = galleryItems
+  .map(
+    ({ preview, description, original }) =>
+      `<li class = "gallery__item">
+      <img  class="gallery__image" 
+      src="${preview}" 
+      alt="${description} 
+       data-source=${original}">
+    </li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+gallery.addEventListener('click', onClick);
+
+function onClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains('.gallery__item')) {
+    return;
+  }
+const instance = basicLightbox.create(`<img src="${evt.target.dataset.source}"`)
+
+instance.show()
+  
+}
 
 
-const markup = galleryItems.map(
-  ({ description, original, preview }) => `<li class="gallery__item">
-<a class="gallery__link" href=${original}
-  <img
-    class="gallery__image"
-    src=${preview}
-    data-source=${original}
-    alt=${description}
-  />
-</a>
-</li>`
-);
 
-gallery.insertAdjacentHTML("beforeend", markup.join(""));
-
-//
